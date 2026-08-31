3e5ff09a1b2fd71c687014dabfaa6d40a01aae64
Wolfigg <98302509+Wolfigg@users.noreply.github.com>
2026-08-31 13:55:11 +0300

    Delete removed planner frontend asset

--- web/assets/planner_v3.js
+++
@@ -1,7 +0,0 @@
-(() => {
-  "use strict";
-  // Session planning was removed from the public AFK board. This file remains as
-  // a compatibility asset for the current site builder and can be deleted in a
-  // later asset-cleanup change without affecting AFK method ranking or market
-  // capacity calculations.
-})();
